c124c4981714d2f9aebc467873eb028a98379bd7
Lokendra Singh <136624583+Lokendrasingh2003@users.noreply.github.com>
2026-08-21 13:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (68.81%) - LeetSync

---
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+ // hello
+var secondHighest = function(s) {
+   let largest = -1 
+   let secondLargest = -1 
+   for(let ch of s){
+     if(!(ch>='a' && ch<='z')){
+        let digit = parseInt(ch)
+        if(digit>largest){
+            secondLargest = largest 
+            largest = digit
+        }
+        else if(digit<largest && digit>secondLargest){
+            secondLargest = digit
+        }
+     }
+   }
+   return secondLargest
+    
+};
