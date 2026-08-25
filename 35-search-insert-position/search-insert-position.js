7e4b18e096e074689fb6411b68119c9d4fd485ac
Lokendra Singh <136624583+Lokendrasingh2003@users.noreply.github.com>
2026-08-25 18:53:14 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (57.12%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left=0
+    let right=nums.length-1 
+    let ans = nums.length
+    while(left<=right){
+        let mid = Math.floor(left+(right-left)/2)
+         if(nums[mid]>=target){
+              right = mid-1 
+               ans = mid
+         }
+         else{
+            left = mid+1 
+         }
+        }
+    
+    return ans
+}
+    
