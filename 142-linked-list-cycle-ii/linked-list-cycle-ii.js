7fc167759bb0034a8e27bcfe231bc33e60d1ad3c
Lokendra Singh <136624583+Lokendrasingh2003@users.noreply.github.com>
2026-08-31 01:48:13 +0530

    Time: 44 ms (79.89%) | Memory: 57.6 MB (41.51%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let slow = head 
+    let fast = head 
+    while(fast!=null && fast.next!=null){
+        slow = slow.next 
+        fast = fast.next.next 
+        if(slow==fast){
+            let entry = head 
+            while(entry!==slow){
+                entry = entry.next 
+                slow = slow.next
+            }
+            return entry
+        }
+    }
+    return null
+    
+};
