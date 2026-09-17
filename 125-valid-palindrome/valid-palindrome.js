46b11a0b78ca8a9e1d087c46182f2f2fb1e5d722
Lokendra Singh <136624583+Lokendrasingh2003@users.noreply.github.com>
2026-09-17 08:14:31 +0530

    Time: 3 ms (86.27%) | Memory: 61.3 MB (20.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,26 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-
-
-
-
 var isPalindrome = function(s) {
-    let str=s.toLowerCase().replace(/[^a-z0-9]/g,'')
-    let left=0
-    let right=str.length-1
     
-    
-    while(left<right){
-        if(str[left]!=str[right]){
-            return false
-        }
-        else{
-            left++
-            right--
-        
-        }
+    s=s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase()
+    let n = s.length
+    let a = check(0,s,n)
+    if(a){
+        return true
     }
-    return true
+    return false
+
     
 };
+
+function check(i,s,n){
+    if(i>=Math.floor(n/2)) return true
+    if(s[i]!=s[n-i-1]) return false 
+    return check(i+1,s,n)
+
+}
