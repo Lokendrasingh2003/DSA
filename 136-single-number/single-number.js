d2978b49f548de20cdd073e00c23665692dbe6b2
Lokendra Singh <136624583+Lokendrasingh2003@users.noreply.github.com>
2026-09-21 15:46:55 +0530

    Time: 1 ms (72.49%) | Memory: 57.7 MB (32.45%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+
+
+var singleNumber = function(nums) {
+    let result = 0 
+    for(let num of nums){
+        result = result^num
+    }
+    return result
+    
+};
