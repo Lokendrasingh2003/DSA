76f6b61c7446a06e9f1b4d1ee83773477bc67331
Lokendra Singh <136624583+Lokendrasingh2003@users.noreply.github.com>
2026-09-25 23:27:39 +0530

    Time: 1 ms (92.24%) | Memory: 65 MB (27.76%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    let five = 0 
+    let ten = 0
+    for(let i=0;i<bills.length;i++){
+        if(bills[i]==5){
+            five++
+        }
+        else if(bills[i]==10){
+            if(five){
+                five--
+                ten++
+            }
+            else{
+                return false
+            }
+        }
+        else{
+            if(ten && five){
+                ten--
+                five--
+            }
+            else if(five>=3){
+                five = five-3 
+            }
+            else{
+                return false
+            }
+        }
+    }
+    return true
+    
+};
