c064a5b26f2ac4f893de1f13ad7395fffa93df92
Lokendra Singh <136624583+Lokendrasingh2003@users.noreply.github.com>
2026-09-25 23:03:43 +0530

    Time: 27 ms (38.14%) | Memory: 61.8 MB (66.19%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a,b)=>a-b)
+    s.sort((a,b)=>a-b)
+    let l=0
+    let r=0
+
+    while(l<=s.length){
+        if(s[l]>=g[r]){
+            r++
+        }
+        l++
+    }
+    return r
+    
+    
+};
